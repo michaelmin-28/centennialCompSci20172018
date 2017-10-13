e80a8937f2dc1f1995df3d2e8392e151b4df9ff3
michaelmin-28 <michaelmin.28@gmail.com>
2017-10-13 10:45:09 -0600

    The first draft of the OddorEven function.
    This 'OddorEven' function checks for a certain amount of integers, and
    then prints if the integers are odd or even.

---
+++ OddorEven/src/OddOrEvenRunner.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class OddOrEvenRunner {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		fileReader readingFile = new fileReader();
+		readingFile.readerFunction();
+
+	}
+
+	
+}
+
+class fileReader {
+	public fileReader() {
+	}
+	
+	public void readerFunction() throws FileNotFoundException {
+		Scanner file = new Scanner(new File("/home/student/Eclipse/OddorEven/src/lab01data.dat"));
+		
+		int size = file.nextInt();
+		file.nextLine();
+		
+		for (int i=0; i < size; i++) {
+			int num = file.nextInt();
+			
+			if (num % 2 == 0) {
+				System.out.println(num + " is even.");
+			}
+			else if (num % 2 == 1){
+				System.out.println(num + " is odd.");
+			}
+			
+		}
+		file.close();
+		return;
+	}
+	
+}
